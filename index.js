861f44f47371a61d1d4576ec3b863dde5c056342
unknown <ivanrabochiigor@outlook.com>
2022-01-19 21:42:27 +0300

    search section was made

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ $(function(){
         prevArrow: '<button class="arrow__left left"></button>',
         nextArrow: '<button class="arrow__right right"></button>'
     });
+
+    $('.search__tabs-item').on('click', function(e){
+        e.preventDefault();
+
+        $('.search__tabs-item').removeClass('search__tabs-item-active');
+        $('.search__content-item').removeClass('search__content-item-active');
+
+        $(this).addClass('search__tabs-item-active');
+        $($(this).attr('href')).addClass('search__content-item-active');
+    });
 });
