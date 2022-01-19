a755ebb17492b47e79b78cd7e44f549d350488b3
unknown <ivanrabochiigor@outlook.com>
2022-01-19 15:23:20 +0300

    made a banner, added README

---
+++ index.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('.banner-section__slider').slick({
+        dots: true,
+        prevArrow: '<button class="arrow__left left"></button>',
+        nextArrow: '<button class="arrow__right right"></button>'
+    });
+});
