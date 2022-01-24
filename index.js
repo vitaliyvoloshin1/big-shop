f32f874364906092e9ce732666568f9d2ee6c83e
unknown <ivanrabochiigor@outlook.com>
2022-01-24 20:49:36 +0300

    products section was made

--- index.js
+++ index.js
@@ -5,13 +5,24 @@ $(function(){
         nextArrow: '<button class="arrow__right right"></button>'
     });
 
-    $('.search__tabs-item').on('click', function(e){
+    $('.tab').on('click', function(e){
         e.preventDefault();
 
-        $('.search__tabs-item').removeClass('search__tabs-item-active');
-        $('.search__content-item').removeClass('search__content-item-active');
+        $('.tab').removeClass('tab-active');
+        $('.tabs-content').removeClass('tabs-content-active');
 
-        $(this).addClass('search__tabs-item-active');
-        $($(this).attr('href')).addClass('search__content-item-active');
+        $(this).addClass('tab-active');
+        $($(this).attr('href')).addClass('tabs-content-active');
     });
+
+    $('.product-item__favorite').on('click', function() {
+        $(this).toggleClass('product-item__favorite-active')
+    });
+
+    $('.product-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="arrow__left__products  left"></button>',
+        nextArrow: '<button class="arrow__right__produsts right"></button>'
+    })
 });
